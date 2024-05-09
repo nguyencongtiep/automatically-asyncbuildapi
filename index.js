893a3185af6e1c6e110733574caec52cfe30b11b
nguyencongtiep <dorian2501nam1311@outlook.com>
2024-05-09 18:13:09 +0700

    update

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function permute(nums) {
-  const result = [];
-  backtrack([]);
-  return result;
-  function backtrack(current) {
-    if (current.length === nums.length) {
-      result.push([...current]);
-      return;
-    }
-    for (const num of nums) {
-      if (current.includes(num)) continue;
-      current.push(num);
-      backtrack(current);
-      current.pop();
-    }
-  }
-}
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
